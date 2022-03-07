1f3a392b8f530461246a93e2cac24d9b1e2dcf54
Eldad Danladi <eldaddanladi@gmail.com>
2022-03-07 13:38:30 -0600

    direct link

--- blog/src/components/HeaderComponent.tsx
+++ blog/src/components/HeaderComponent.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-let header = "../../assets/maou.jpg"
+let header = "https://www.maoushimazu.tk/blog/assets/maou.jpg"
 
 export default class HeaderComponent extends React.Component<{}> {
     render() {
